60b2b0f65028b30c2639511fb6f5929db48cb0b8
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-10-24 20:13:52 +0000

    isolated

---
+++ main.py
@@ -0,0 +1,10 @@
+from methods_help import method_help, greetings
+# from functions import function
+# from returnStuff import returnS
+
+method_help()
+greetings("thomas")
+greetings("sam")
+greetings("rebecca")
+# function()
+# returnS()
